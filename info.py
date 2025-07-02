592f91eb2ea838bde0ee3ce1cb1fef30b882e6c0
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-07-02 13:04:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'shiv')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://shiv:Mishraji99@@cluster0.txlrokw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://shiv:shiv99@cluster0.bew71ug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/BoB_AreA')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/BoB_Files1')
