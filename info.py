d198f031829ea087cc5df2eb0c94174d359898a7
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-07-04 20:42:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,13 +42,13 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002085372939') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://shiv:shiv99@cluster0.bew71ug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # mongodb+srv://BoB:Mishraji99@cluster0.v99uneo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://BoB:Mishraji99@cluster0.v99uneo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # mongodb+srv://BoB:Mishraji99@cluster0.v99uneo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ITACHI")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'shiv')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://BoB:Mishraji99@cluster0.v99uneo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") #mongodb+srv://shiv:shiv99@cluster0.bew71ug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://shiv:shiv99@cluster0.bew71ug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") #mongodb+srv://shiv:shiv99@cluster0.bew71ug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/BoB_AreA')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/BoB_Files1')
