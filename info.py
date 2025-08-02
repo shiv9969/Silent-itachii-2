0071f4fcab14c3c233e3d0e6bd3fe998a4409d44
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-08-02 17:24:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002085372939') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://BoB:Mishraji99@cluster0.v99uneo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://shiv:shiv@cluster0.nqvmh4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ITACHI")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'shiv')
 
